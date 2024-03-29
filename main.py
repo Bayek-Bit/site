35ced14ae756228eb4ac241620dd8a45a240127f
Bayek-Bit <twink.7w1nk@yandex.ru>
2024-03-29 20:05:26 +0500

    front + back

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Diary"
 )
 
-app.mount('/static', StaticFiles(directory="E:/www/public", html=True))
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "http://localhost:8080",
+]
 
-@app.get("/hello")
-def hello():
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "DELETE", "PATCH", "PUT"],
+    allow_headers=["*"],
+)
+
+app.mount('/', StaticFiles(directory="E:/www/public", html=True))
+
+@app.get("/")
+async def hello():
     return "Hello!"
