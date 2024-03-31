7bc80b153fe20f15131bcc16e5281e9409b27dfc
Bayek-Bit <twink.7w1nk@yandex.ru>
2024-03-31 18:07:49 +0500

    front+back (completely)

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Diary"
 )
 
 origins = [
-    "http://localhost",
-    "http://localhost:5500",
-    "http://localhost:8000",
+    "http://127.0.0.1:5500",
 ]
 
 app.add_middleware(
@@ -17,12 +14,13 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "DELETE", "PATCH", "PUT"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 @app.get("/diary")
-async def get_info():
-    return "INFO"
-
-app.mount('/', StaticFiles(directory="E:/www/public", html=True))
-
+def get_info():
+    return {"name":"John",
+             "grade": "5",
+             "date": "12.03.2000"
+            }
--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
-console.log("xy");
-let closebtn = document.querySelector(".btn-close");
-let diary_div = document.querySelector(".diary");
-
-let diary_btn = document.querySelector(".diary_btn")
-
-closebtn.addEventListener('click', function(event) {
-    diary_div.style.display = 'none';
-});
-
-diary_btn.addEventListener('click', function(event) {
-    event.preventDefault();
-    diary_div.style.display = 'flex';
+document.getElementById("diaryButton").addEventListener("click", function() {
+    fetch("http://127.0.0.1:8000/diary")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Преобразование ответа в JSON
+    })
+    .then(data => {
+        // Очищаем таблицу перед добавлением новых данных
+        const tbody = document.getElementById("diaryTable").getElementsByTagName('tbody')[0];
+        tbody.innerHTML = "";
+        
+        // Добавляем строку в таблицу из полученных данных
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <td>${data.name}</td>
+            <td>${data.grade}</td>
+            <td>${data.date}</td>
+        `;
+        tbody.appendChild(newRow);
+    })
+    .catch(error => {
+        // Обработка ошибок
+        console.error('There has been a problem with your fetch operation:', error);
+    });
 });
