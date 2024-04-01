f35ada92a2854374ad75855742a37226004b5ca9
Bayek-Bit <twink.7w1nk@yandex.ru>
2024-04-01 12:55:47 +0500

    before adding db funcs to main file

--- public/db/config.py
+++ public/db/config.py
@@ -2,3 +2,4 @@ host = "localhost"
 user = "postgres"
 password = "228335"
 db_name = "diary"
+# localhost :)
--- public/db/db.py
+++ public/db/db.py
@@ -1,11 +1,17 @@
-import psycopg2
+import asyncpg
 from config import host, user, password, db_name
 
 class Database_worker:
-    def __init__(self) -> None:
+    def __init__(self, host, user, password, db_name) -> None:
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+    
+    async def connect(self):
         try:
             # Connect to database
-            self.connection = psycopg2.connect(
+            self.connection = await asyncpg.connect(
                 host=host,
                 user=user,
                 password=password,
@@ -14,23 +20,30 @@ class Database_worker:
         except Exception as _ex:
             print(f"[INFO] Error with connection to PG: {_ex}")
             self.connection = None
-        finally:
-            pass
-
-    def check_version(self):
+    
+    async def check_version(self):
         if self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(
-                    "SELECT version();"
-                )
-
-                print(f"Server version: {cursor.fetchone()[0]}")
+            version = await self.connection.fetchval("SELECT version();")
+            print(f"Server version: {version}")
         else:
             print("No connection available.")
 
-    def load_table(self):
-        pass
+    async def get_classes_list(self, teacher_id):
+        if self.connection:
+            # Получаем id классов, которые обучает учитель. Пример: [<Record class_id=1>]
+            classes = await self.connection.fetch("SELECT class_id FROM teachers_users WHERE teacher_id = $1", teacher_id)
+            # Создаём список, который хранит id классов, которые обучает учитель
+            classes_id = []
+            for i in classes:
+                classes_id.append(i["class_id"])
+            return classes_id
+
+
+async def main():
+    db = Database_worker(host=host, user=user, password=password, db_name=db_name)
+    await db.connect()
+    await db.get_classes_list(teacher_id=1)
 
 if __name__ == "__main__":
-    db = Database_worker()
-    db.check_version()
+    import asyncio
+    asyncio.run(main())
